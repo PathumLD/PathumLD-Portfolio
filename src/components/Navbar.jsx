2e45932c33f7cb38e187010043c4b39fd067b3e8
Pathum Lakshan Dissanayake <iamartseeker@gmail.com>
2024-10-20 01:19:11 +0530

    Home edited

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import { memo } from 'react';
-import img1 from '../assets/react.svg';
 import { useNavigate } from 'react-router-dom';
+import { HiOutlineBars3BottomRight } from "react-icons/hi2";
+import { MdOutlineClose } from "react-icons/md";
 
 const Navbar = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -16,65 +17,51 @@ const Navbar = () => {
   };
 
   return (
-    <nav className=' container mx-auto max-w-lg sm:max-w-xl md:max-w-3xl lg:max-w-5xl xl:max-w-6xl'>
+    <nav className='container flex items-center justify-center  md:mx-auto  max-w-lg sm:max-w-xl md:max-w-3xl lg:max-w-5xl xl:max-w-6xl'>
       <div className='bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg border border-black border-opacity-20 rounded-full'>
         <div className='py-4 mx-auto'>
-          <div className='flex justify-between mx-20'>
+          <div className='flex justify-between items-center gap-60 md:gap-[40rem] mx-10'>
+            {/* Logo and title */}
             <button onClick={toggleLogo}>
-              <img src={img1} alt='logo' />
+              <h1 className='font-bold text-2xl text-cyan-400'>PathumLD</h1>
             </button>
-            <div className='absolute inset-y-0 right-12 flex items-center sm:hidden'>
+            {/* Mobile menu icon */}
+            <div className='sm:hidden'>
               <button
                 onClick={toggleModal}
-                className='inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'
+                className='inline-flex items-center justify-center p-2 rounded-md text-cyan-400 hover:text-cyan-600'
                 aria-controls='mobile-menu'
                 aria-expanded='false'
               >
                 {!isModalOpen ? (
-                  <svg
-                    className='block h-6 w-6'
-                    xmlns='http://www.w3.org/2000/svg'
-                    fill='none'
-                    viewBox='0 0 24 24'
-                    stroke='currentColor'
-                    aria-hidden='true'
-                  >
-                    <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M4 6h16M4 12h16m-7 6h7' />
-                  </svg>
+                  <HiOutlineBars3BottomRight className="h-6 w-6" />
                 ) : (
-                  <svg
-                    className='block h-6 w-6'
-                    xmlns='http://www.w3.org/2000/svg'
-                    fill='none'
-                    viewBox='0 0 24 24'
-                    stroke='currentColor'
-                    aria-hidden='true'
-                  >
-                    <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M6 18L18 6M6 6l12 12' />
-                  </svg>
+                  <MdOutlineClose className="h-6 w-6" />
                 )}
               </button>
             </div>
+
+            {/* Desktop menu */}
             <div className='hidden sm:block sm:ml-6'>
               <div className='flex space-x-4'>
                 <ul className='flex justify-end gap-6'>
                   <li>
-                    <a href='#home' className='text-teal-200 underline-animation'>
+                    <a href='#home' className='text-cyan-400 underline-animation'>
                       Home
                     </a>
                   </li>
                   <li>
-                    <a href='#about' className='text-teal-200 underline-animation'>
+                    <a href='#about' className='text-cyan-400 underline-animation'>
                       About
                     </a>
                   </li>
                   <li>
-                    <a href='#projects' className='text-teal-200 underline-animation'>
+                    <a href='#projects' className='text-cyan-400 underline-animation'>
                       Projects
                     </a>
                   </li>
                   <li>
-                    <a href='#contact' className='text-teal-200 underline-animation'>
+                    <a href='#contact' className='text-cyan-400 underline-animation'>
                       Contact
                     </a>
                   </li>
@@ -84,21 +71,23 @@ const Navbar = () => {
           </div>
         </div>
       </div>
+
+      {/* Mobile menu */}
       {isModalOpen && (
         <div className='sm:hidden' id='mobile-menu'>
-          <div className='bg-gray-100 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-3xl h-72 mt-6'>
+          <div className='bg-black bg-opacity-20 backdrop-filter backdrop-blur-xl rounded-3xl h-72 mt-4'>
             <div className='px-2 pt-2 pb-3 text-center'>
               <div className='flex flex-col items-center space-y-4 mt-8 justify-center'>
-                <a href='#home' className='block px-3 py-2 rounded-md text-base font-medium text-stone-500 hover:text-stone-600 underline-animation'>
+                <a href='#home' className='block px-3 py-2 rounded-md text-base font-medium text-cyan-400 hover:text-cyan-300 underline-animation'>
                   Home
                 </a>
-                <a href='#about' className='block px-3 py-2 rounded-md text-base font-medium text-stone-500 hover:text-stone-600 underline-animation'>
+                <a href='#about' className='block px-3 py-2 rounded-md text-base font-medium text-cyan-400 hover:text-cyan-300 underline-animation'>
                   About
                 </a>
-                <a href='#projects' className='block px-3 py-2 rounded-md text-base font-medium text-stone-500 hover:text-stone-600 underline-animation'>
+                <a href='#projects' className='block px-3 py-2 rounded-md text-base font-medium text-cyan-400 hover:text-cyan-300 underline-animation'>
                   Projects
                 </a>
-                <a href='#contact' className='block px-3 py-2 rounded-md text-base font-medium text-stone-500 hover:text-stone-600 underline-animation'>
+                <a href='#contact' className='block px-3 py-2 rounded-md text-base font-medium text-cyan-400 hover:text-cyan-300 underline-animation'>
                   Contact
                 </a>
               </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -49,7 +49,7 @@ const Home = () => {
 
   return (
     <section id='home' className="h-screen flex items-center justify-center p-6 sm:p-12 md:p-20 bg-slate-950">
-      <div className="container max-w-5xl mx-auto flex flex-col md:flex-row items-center justify-center gap-8 md:gap-28 mt-32 md:mt-24">
+      <div className="container max-w-5xl mx-auto flex flex-col md:flex-row items-center justify-center gap-8 md:gap-28 mt-20 ">
         
         {/* Left Div: Profile Image */}
         <div className="flex justify-center md:justify-end relative left-div">
@@ -57,7 +57,7 @@ const Home = () => {
             {/* Responsive SVG Dotted Border */}
             <svg
               viewBox="0 0 200 200"
-              className="w-[calc(40vw+8px)] h-[calc(40vw+8px)] md:w-[calc(20vw+8px)] md:h-[calc(20vw+8px)] lg:w-[calc(24vw+8px)] lg:h-[calc(24vw+8px)] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 svg-border"
+              className="w-[calc(32vw+8px)] h-[calc(32vw+8px)] md:w-[calc(20vw+8px)] md:h-[calc(20vw+8px)] lg:w-[calc(24vw+8px)] lg:h-[calc(24vw+8px)] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 svg-border"
             >
               <circle 
                 cx="100" 
@@ -74,7 +74,7 @@ const Home = () => {
               <img 
                 src={myImage} 
                 alt="Profile" 
-                className="w-[40vw] h-[40vw] md:w-[20vw] md:h-[20vw] lg:w-[24vw] lg:h-[24vw] rounded-full mx-auto"
+                className="w-[32vw] h-[32vw] md:w-[20vw] md:h-[20vw] lg:w-[24vw] lg:h-[24vw] rounded-full mx-auto"
               />
             </div>
           </div>
@@ -83,11 +83,11 @@ const Home = () => {
         {/* Right Div: Text and Typewriter */}
         <div className="roadmap-item text-center md:text-left right-div flex-1">
           <h1 className="text-2xl md:text-4xl text-white ">Hello, I&rsquo;m</h1>
-          <h1 className="text-4xl md:text-6xl text-emerald-400 font-bold -ml-1">Pathum Dissanayake </h1>
+          <h1 className="text-4xl md:text-6xl text-cyan-400 font-bold -ml-1">Pathum Dissanayake </h1>
           <div className="text-2xl md:text-4xl text-blue-400 flex items-center justify-center md:justify-start">
             <h2 className="text-lg md:text-3xl text-white inline-flex items-center">
                and I&rsquo;m a&nbsp; {/* Ensures the "I'm" stays on the same line */}
-              <span className='text-emerald-300 inline-block'>
+              <span className='text-cyan-400 inline-block'>
                 {roles.map((role, index) => (
                   <Fragment key={index}>
                     {currentRoleIndex === index && (
@@ -107,16 +107,16 @@ const Home = () => {
               </span>
             </h2>
           </div>
-          <p className="text-sm md:text-xl mt-4 md:mt-8 text-justify text-white">Where logic meets creativity, I build digital dreams. My code paints, and my designs compute.</p>
-          <div className='flex gap-4 items-center justify-center lg:justify-start'>
+          <p className="text-sm md:text-xl mt-2 mx-12 md:mx-auto md:mt-8 text-justify text-white">Where logic meets creativity, I build digital dreams. My code paints, and my designs compute.</p>
+          <div className='flex gap-4 items-center justify-center lg:justify-start mt-4'>
             <div className='mt-4 flex gap-4 text-xl text-white'>
-              <a href="https://www.linkedin.com/in/pathumld/" className='border p-2 rounded-full hover:text-emerald-400 hover:border-emerald-400'><FaLinkedinIn /></a>
-              <a href="https://github.com/PathumLD" className='border p-2 rounded-full hover:text-emerald-400 hover:border-emerald-400'><FaGithub /></a>
-              <a href="mailto:pathumlk.diz@gmail.com" className='border p-2 rounded-full hover:text-emerald-400 hover:border-emerald-400'><MdOutlineAlternateEmail /></a>
-              <a href="https://wa.me/+94767342605" className='border p-2 rounded-full hover:text-emerald-400 hover:border-emerald-400'><FaWhatsapp /></a>
+              <a href="https://www.linkedin.com/in/pathumld/" className='border p-2 rounded-full hover:text-cyan-400 hover:border-cyan-400 '><FaLinkedinIn /></a>
+              <a href="https://github.com/PathumLD" className='border p-2 rounded-full hover:text-cyan-400 hover:border-cyan-400'><FaGithub /></a>
+              <a href="mailto:pathumlk.diz@gmail.com" className='border p-2 rounded-full hover:text-cyan-400 hover:border-cyan-400'><MdOutlineAlternateEmail /></a>
+              <a href="https://wa.me/+94767342605" className='border p-2 rounded-full hover:text-cyan-400 hover:border-cyan-400'><FaWhatsapp /></a>
             </div>
             <a href="/cv.pdf" download>
-              <button className="mt-4 px-4 py-2 btn-animate text-white rounded-3xl border hover:text-emerald-400 hover:border-emerald-400 shadow-lg transition duration-300">
+              <button className="mt-4 px-4 py-2 btn-animate text-white rounded-3xl border hover:text-cyan-400 hover:border-cyan-400 shadow-lg transition duration-300">
                 Download CV
               </button>
             </a>
