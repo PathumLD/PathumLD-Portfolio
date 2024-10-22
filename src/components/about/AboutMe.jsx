f49ea4bbbe683940d1dc4a31807c553ff81bb61a
Pathum Lakshan Dissanayake <iamartseeker@gmail.com>
2024-10-22 20:31:49 +0530

    About me changed

--- src/components/about/AboutMe.jsx
+++ src/components/about/AboutMe.jsx
@@ -7,17 +7,17 @@ const AboutMe = () => {
             <img src={Image2} className='rounded-xl p-4 border-2  border-cyan-400' alt="Image2" />
         </div>
        <div className='text-white my-auto pt-10 md:pt-0 '>
-            <h2 className="text-4xl font-bold my-3 mx-auto">Pathum Dissanayake</h2>
-            <p className='mb-3'>
+            {/* <h2 className="text-3xl md:text-4xl font-bold my-3 text-center mx-auto">Pathum Dissanayake</h2> */}
+            <p className='mb-12  text-center font semibold'>
                 I&rsquo;m a passionate software engineer with experience in building
                 scalable web applications using modern technologies.
             </p>
-            <div className=''>
-                <h3 className=''><span className='font-bold'>Full Name :</span> <span className='ml-2'>Dissanayake Pathiranage Pathum Lakshan Dissanayake</span></h3>
-                <h3 className=''><span className='font-bold'>Address :</span> <span className='ml-2'>Kurunegala, Sri Lanka</span></h3>
-                <h3 className=''><span className='font-bold'>Phone :</span> <span className='ml-2'>+94 76 734 2605 / +94 72 999 7763</span></h3>
-                <h3 className=''><span className='font-bold'>Email :</span> <span className='ml-2'>pathumlk.diz@gmail.com</span></h3>
-                <h3 className=''><span className='font-bold'>Languages :</span> <span className='ml-2'>English, Sinhalese</span></h3>
+            <div className='flex-row mx-4 '>
+                <h3 className=''><span className='font-bold text-cyan-300'>Full Name :</span> <span className='ml-2'>Dissanayake Pathiranage Pathum Lakshan Dissanayake</span></h3>
+                <h3 className=''><span className='font-bold text-cyan-300'>Address :</span> <span className='ml-2'>Kurunegala, Sri Lanka</span></h3>
+                <h3 className=''><span className='font-bold text-cyan-300'>Phone :</span> <span className='ml-2'>+94 76 734 2605 / +94 72 999 7763</span></h3>
+                <h3 className=''><span className='font-bold text-cyan-300'>Email :</span> <span className='ml-2'>pathumlk.diz@gmail.com</span></h3>
+                <h3 className=''><span className='font-bold text-cyan-300'>Languages :</span> <span className='ml-2'>English, Sinhalese</span></h3>
             </div>
        </div>
       </div>
