d6511fdf7f5cc256bc72ed114b3588ed91cc64a7
PathumLD <pathumlk10158@gmail.com>
2025-05-13 12:25:57 +0530

    Added certificates

--- src/data/CertificateData.js
+++ src/data/CertificateData.js
@@ -6,6 +6,7 @@ export const certificatesData = [
       link: 'https://www.linkedin.com/posts/pathumld_web-design-for-beginners-activity-7071789407401361408-E6Un?utm_source=share&utm_medium=member_desktop',
       date: 'June 2023',
     },
+
     {
       id: 2,
       name: '.Net Fullstack Foundation',
@@ -13,6 +14,7 @@ export const certificatesData = [
       link: 'https://coursera.org/share/eeba672b0e6cfdb0e74ad7b9c3c1a474',
       date: 'Mar 2024',
     },
+
     {
       id: 3,
       name: 'Building Web APIs with ASP.Net Core',
@@ -20,6 +22,7 @@ export const certificatesData = [
       link: 'https://www.linkedin.com/posts/pathumld_certificate-of-completion-activity-7148395656703791104-sd5y?utm_source=share&utm_medium=member_desktop',
       date: 'Jan 2024',
     },
+
     {
       id: 4,
       name: 'Introduction to JavaScript',
@@ -27,6 +30,7 @@ export const certificatesData = [
       link: 'https://www.linkedin.com/posts/pathumld_introduction-to-javascript-activity-7071789945169850368-OJUn?utm_source=share&utm_medium=member_desktop',
       date: 'Jun 2023',
     },
+
     {
       id: 5,
       name: 'SQL Basic',
@@ -34,6 +38,7 @@ export const certificatesData = [
       link: 'https://www.hackerrank.com/certificates/4babb86f615d',
       date: 'Jun 2023',
     },
+
     {
       id: 6,
       name: 'SQL Intermediate',
@@ -41,6 +46,7 @@ export const certificatesData = [
       link: 'https://www.hackerrank.com/certificates/554c5d0c45f6',
       date: 'Jun 2023',
     },
+
     {
       id: 7,
       name: 'Introduction to Cyber Security',
@@ -48,6 +54,7 @@ export const certificatesData = [
       link: 'https://www.credly.com/badges/02c1a29f-a5a1-469b-ba8a-e1a7c3894d68?source=linked_in_profile',
       date: 'Jul 2021',
     },
+
     {
       id: 8,
       name: 'Cyber Security Essentials',
@@ -55,6 +62,7 @@ export const certificatesData = [
       link: 'https://www.credly.com/badges/0c1f1c08-3365-4366-83f9-0578d4d745f1?source=linked_in_profile',
       date: 'Mar 2022',
     },
+
     {
       id: 9,
       name: 'Career Essentials in Cybersecurity',
@@ -62,6 +70,7 @@ export const certificatesData = [
       link: 'https://www.linkedin.com/posts/pathumld_certificate-activity-7162897416189362176-2GQq?utm_source=share&utm_medium=member_desktop',
       date: 'Feb 2024',
     },
+
     {
       id: 10,
       name: 'DevOps Foundations: DevSecOps',
@@ -69,5 +78,22 @@ export const certificatesData = [
       link: 'https://www.linkedin.com/posts/pathumld_certificate-of-completion-activity-7213607869223428096-1rdq?utm_source=share&utm_medium=member_desktop',
       date: 'Jul 2024',
     },
+
+    {
+      id: 11,
+      name: 'Cloud Computing',
+      issuer: 'CoDeKu DevOps Academy',
+      link: 'https://www.linkedin.com/posts/pathumld_cloudcomputing-containerization-devops-activity-7326573099204628480-HsKJ?utm_source=share&utm_medium=member_desktop&rcm=ACoAADbSQqUBAvBBb56O1Zk6240l5m1Z7jq2CfU',
+      date: 'Apr 2025',
+    },
+
+    {
+      id: 12,
+      name: 'Introduction to Containerization',
+      issuer: 'CoDeKu DevOps Academy',
+      link: 'https://www.linkedin.com/posts/pathumld_cloudcomputing-containerization-devops-activity-7326573099204628480-HsKJ?utm_source=share&utm_medium=member_desktop&rcm=ACoAADbSQqUBAvBBb56O1Zk6240l5m1Z7jq2CfU',
+      date: 'Apr 2025',
+    },
+
   ];
   
